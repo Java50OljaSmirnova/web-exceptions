84aa80caaeb0e6f8dac61a4365446651d9126092
oljas <olja.smirnov93@gmail.com>
2024-04-10 23:12:15 +0300

    fix account

--- src/main/java/telran/exceptions/AccountNotFoundException.java
+++ src/main/java/telran/exceptions/AccountNotFoundException.java
@@ -5,7 +5,7 @@ import telran.messages.ExceptionMessages;
 @SuppressWarnings("serial")
 public class AccountNotFoundException extends NotFoundException {
 
-	public AccountNotFoundException(String message) {
+	public AccountNotFoundException() {
 		super(ExceptionMessages.ACCOUNT_NOT_FOUND);
 	}
 
